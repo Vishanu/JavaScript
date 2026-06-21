ee78f8065a15b7545acc38aa23a9558028bf4591
Vishanu Giri <vishanugiri2002@gmail.com>
2026-06-21 19:34:56 +0530

    **Operators**

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -22,7 +22,34 @@ let score = false;
 //"giri" => true
 
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(typeof stringNumber);
-console.log(stringNumber);
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
+
+
+
+// *********************** Operations*******************
+// let value = 3;
+// let negValue = -value
+// console.log(negValue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%3);
+
+// console.log("1" + 2);
+// console.log(1+2+"2");
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1, num2, num3
+// num1 = num2 = num3 = 2+1
+// console.table([num1,num2,num3]);
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter);
